c5d4da8537ede451ee7737460b820ccf48d9364d
Shreeram Samartha <satyajitdhore5@gmail.com>
2023-05-07 08:41:58 +0530

    Configuration File

---
+++ config.py
@@ -0,0 +1,10 @@
+
+import os
+
+csv_file_path = "file.csv"
+
+port_number = 5005
+
+MODEL_FILE_PATH = os.path.join("project_app", "Linear model.pkl")
+
+JSON_FILE_PATH = os.path.join("project_app", "Project_data.json")
